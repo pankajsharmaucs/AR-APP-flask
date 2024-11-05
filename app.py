cb577017df8b5e4311a3fb133e31fd04017b2b4a
ps <ps@gmail.com>
2024-11-05 12:54:48 +0530

    v6

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT environment variable from Render
+    port = int(os.environ.get("PORT", 5000))
+    # Set the host to '0.0.0.0' for production
+    app.run(host='0.0.0.0', port=port)
